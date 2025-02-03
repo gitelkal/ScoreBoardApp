530c030a28ac0cbb77f97448f5b45439d26c46d2
Akkely <112615486+Akkely@users.noreply.github.com>
2025-02-03 12:14:34 +0100

    Start frontend

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -4,8 +4,19 @@ import { RouterOutlet } from '@angular/router';
 @Component({
   selector: 'app-root',
   imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+    template: `
+  <main>
+
+    <header class="brand-name">
+      <img class="brand-logo" src="/assets/logo.svg" alt="logo" aria-hidden="true" />
+    </header>
+
+  <section class="content">
+
+  </section>
+</main>
+`,
+styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'client';
