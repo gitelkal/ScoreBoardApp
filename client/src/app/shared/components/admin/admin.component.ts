5723cc4ee39a36a1d8686562477ba1c4339024d3
ashnai <soffiloffan81@hotmail.com>
2025-02-12 16:23:16 +0100

    Admin component

--- client/src/app/shared/components/admin/admin.component.ts
+++ client/src/app/shared/components/admin/admin.component.ts
@@ -1,11 +1,27 @@
-import { Component } from '@angular/core';
+import { AdminService } from '@app/core/services/AdminService/admin.service';
+import { NgIf, NgFor, AsyncPipe } from '@angular/common';
+import { Component, inject } from '@angular/core';
+import { Router } from '@angular/router';
+
 
 @Component({
   selector: 'app-admin',
-  imports: [],
+  standalone: true,
+  imports: [NgIf, NgFor, AsyncPipe ],
+  providers: [AdminService],
   templateUrl: './admin.component.html',
   styleUrl: './admin.component.css'
 })
 export class AdminComponent {
+  adminService = inject(AdminService);
+  
+  getAllAdmins$ = this.adminService.getAllAdmins();
 
+  constructor(private router: Router) {} // Injecta router för navigering
+
+  handleAdminClick(admin: any) {
+    console.log('Admin clicked:', admin);
+    // Navigera till en admin-detaljsida
+    this.router.navigate(['/admin', admin.adminID]);
+  }
 }
