5ac4af02b04d20e290188b716b80ed98992252a4
ludwigstrand <ludwig.strand@live.com>
2025-02-13 14:47:12 +0100

    Ersatt mat-card, förbättrat header och home något

--- server/obj/Debug/net8.0/server.AssemblyInfo.cs
+++ server/obj/Debug/net8.0/server.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("server")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+66797a4796a6a4b746073215f976fbe67e23705c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e4ea329becc92452d8df99aa117ec460c9cb7dc7")]
 [assembly: System.Reflection.AssemblyProductAttribute("server")]
 [assembly: System.Reflection.AssemblyTitleAttribute("server")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
