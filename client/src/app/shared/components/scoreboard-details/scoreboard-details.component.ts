965828ac6a7dd19fa323499531d67a2ccff77741
nnaha313 <112633436+nnaha313@users.noreply.github.com>
2025-02-17 13:19:49 +0100

    Rätt scoreboard visas i detalj när man klickar på det från home

--- client/src/app/shared/components/scoreboard-details/scoreboard-details.component.ts
+++ client/src/app/shared/components/scoreboard-details/scoreboard-details.component.ts
@@ -1,11 +1,37 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, inject } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { ScoreboardService } from '@app/core/services/scoreboardService/scoreboard.service';
+import { SignalRService } from '@app/core/services/signalRService/signal-r.service';
+import { Observable, BehaviorSubject } from 'rxjs';
+import { Scoreboards } from '@app/shared/models/scoreboards.model';
+import { ScoreboardTeams } from '@app/shared/models/scoreboardTeams.model';
+import { AsyncPipe, NgIf, NgFor } from '@angular/common';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-scoreboard-details',
-  imports: [],
+  standalone: true,
+  imports: [NgIf, NgFor, AsyncPipe, CommonModule],
   templateUrl: './scoreboard-details.component.html',
-  styleUrl: './scoreboard-details.component.css'
+  styleUrls: ['./scoreboard-details.component.css']
 })
-export class ScoreboardDetailsComponent {
+export class ScoreboardDetailsComponent implements OnInit {
+  scoreboard$!: Observable<Scoreboards>;
+  teams$ = new BehaviorSubject<ScoreboardTeams[]>([]); // Store teams as an observable
+
+  scoreboardService = inject(ScoreboardService);
+
+  constructor(private route: ActivatedRoute, private signalRService: SignalRService) {}
+
+  ngOnInit() {
+    this.route.paramMap.subscribe(params => {
+      const id = params.get('id');
+      if (id) {
+        this.scoreboard$ = this.scoreboardService.getOneScoreboard(id);
+      }
+    });
+
+
+  }
 
 }
