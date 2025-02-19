329317fedb8537715fd7f55736fc5657b235f560
NordicByte <h22elkal@du.se>
2025-02-19 11:13:34 +0100

    fixat rich details

--- client/src/app/shared/components/scoreboard-details/scoreboard-details.component.spec.ts
+++ client/src/app/shared/components/scoreboard-details/scoreboard-details.component.spec.ts
@@ -1,5 +1,4 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
 import { ScoreboardDetailsComponent } from './scoreboard-details.component';
 
 describe('ScoreboardDetailsComponent', () => {
--- client/src/app/shared/models/richScoreboard.model.ts
+++ client/src/app/shared/models/richScoreboard.model.ts
@@ -1,9 +1,46 @@
-import { Teams } from "./teams.models";
+
 
 export interface RichScoreboard {
     scoreboardId: number;
     name: string;
     startedAt: Date;
-    endedAt: Date;
+    endedAt?: Date; // Optional since not all scoreboards may have an end date
     active: boolean;
+    teams: RichTeam[];  
 }
+
+export interface RichTeam {
+    teamId: number;
+    teamName: string;
+    points: number;
+    lastUpdated: Date;
+    users: RichUser[];
+}
+
+export interface RichUser {
+    userId: number;
+    userName: string;
+}
+
+
+
+export interface ScoreboardTeams {
+    scoreboardTeamId: number;
+    scoreboardId: number;
+    teamId: number;
+    points: number;
+    lastUpdated: Date;
+    users: TeamUsers[]; // Include users inside teams
+}
+
+export interface TeamUsers {
+    teamUserId: number;
+    teamId: number;
+    userId: number;
+}
+
+export interface ScoreboardResponse {
+    scoreboard: RichScoreboard;
+    teams: ScoreboardTeams[];
+}
+
