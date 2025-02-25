d957c858d0953dd55af046b479ef91fc2f886b3f
nnaha313 <112633436+nnaha313@users.noreply.github.com>
2025-02-25 22:55:35 +0100

    Alla lag syns under "Lag"

--- client/src/app/shared/components/team/team.component.ts
+++ client/src/app/shared/components/team/team.component.ts
@@ -1,11 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { TeamService } from '@app/core/services/teamService/team.service';
+import { ScoreboardService } from '@app/core/services/scoreboardService/scoreboard.service';
+import { NgIf, NgFor, AsyncPipe } from '@angular/common';
+import { ActivatedRoute } from '@angular/router';
+
 
 @Component({
   selector: 'app-team',
-  imports: [],
+  standalone: true,
+  imports: [NgIf, NgFor, AsyncPipe],
   templateUrl: './team.component.html',
   styleUrl: './team.component.css'
 })
 export class TeamComponent {
 
+  teamService = inject(TeamService);
+  getAllTeams$ = this.teamService.getAllTeams();
+  getOneTeam$ = this.teamService.getOneTeam('some-id');
+
+  scoreboardService = inject(ScoreboardService);
+  
+  route = inject(ActivatedRoute);
+
 }
+
