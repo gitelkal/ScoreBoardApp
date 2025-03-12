893d7178cd0170904c9d6434472546c70b7eb72e
ludwigstrand <ludwig.strand@live.com>
2025-03-12 13:15:01 +0100

    Filtrera tavlor i historik

--- client/src/app/shared/components/scoreboards-history/scoreboards-history.component.ts
+++ client/src/app/shared/components/scoreboards-history/scoreboards-history.component.ts
@@ -1,14 +1,15 @@
 import { Component, inject, OnInit } from '@angular/core';
 import { ScoreboardService } from '@app/core/services/scoreboardService/scoreboard.service';
-import { NgFor, DatePipe } from '@angular/common';
+import { NgIf, NgFor, DatePipe } from '@angular/common';
 import { RouterLink, Router, NavigationEnd} from '@angular/router';
 import { map } from 'rxjs/operators';
 import { Scoreboards } from '@app/shared/models/scoreboards.model';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-scoreboards-history',
   standalone: true,
-  imports: [NgFor, DatePipe, RouterLink],
+  imports: [NgFor, NgIf, DatePipe, RouterLink, FormsModule],
   providers: [ScoreboardService],
   templateUrl: './scoreboards-history.component.html',
   styleUrls: ['./scoreboards-history.component.css']
@@ -16,7 +17,9 @@ import { Scoreboards } from '@app/shared/models/scoreboards.model';
 export class ScoreboardsHistoryComponent implements OnInit {
   scoreboardService = inject(ScoreboardService);
   router = inject(Router);
+  query: string = '';
   inactiveScoreboards: Scoreboards[] = [];
+  filteredScoreboards: Scoreboards[] = [];
 
   ngOnInit(): void {
     this.loadScoreboards();
@@ -32,6 +35,16 @@ export class ScoreboardsHistoryComponent implements OnInit {
       map(scoreboards => scoreboards.filter(scoreboard => !scoreboard.active))
     ).subscribe(filteredScoreboards => {
       this.inactiveScoreboards = filteredScoreboards;
+      this.filterScoreboards();
     });
+  }
+  filterScoreboards(): void {
+    if (this.query) {
+      this.filteredScoreboards = this.inactiveScoreboards.filter(scoreboard =>
+        scoreboard.name.toLowerCase().includes(this.query.toLowerCase())
+      );
+    } else {
+      this.filteredScoreboards = [...this.inactiveScoreboards];
+    }
   }
 }
